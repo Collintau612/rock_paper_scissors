73c5c5670ff9ac64808f1d1460408ce24e9778e5
Lambchopthekid <collinwbsanford@gmail.com>
2022-08-22 13:40:54 -0400

    add const variables

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //store variable "rock"
+const rock = "rock"
+
 //store variable "paper"
+const paper= "paper"
+
 //store variable "scissors"
+const scissors = "scissors"
+
 //randomly return one of the three variables
